d18723344876b4d975fa70b864a11c9be29eb4f0
Guadalupe Cisneros <guadalupecisneros@hartnell.edu>
2018-10-15 13:55:39 -0700

    Simple Flashcards App1

--- flashcards/ViewController.swift
+++ flashcards/ViewController.swift
@@ -1,25 +1,29 @@
-//
 //  ViewController.swift
 //  flashcards
 //
 //  Created by Guadalupe Cisneros on 10/13/18.
 //  Copyright © 2018 Guadalupe Cisneros. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
+  
+    @IBOutlet weak var frontview: UILabel!
+    @IBOutlet weak var backlabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+        frontview.isHidden = true
+    }
+    @IBOutlet var tapgesture: UITapGestureRecognizer!
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
